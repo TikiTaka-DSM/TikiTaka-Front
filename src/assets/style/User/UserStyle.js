374a4ea5875a4a41c2e87cfe83f2f411e2735071
김해린 <rin3583@naver.com>
2020-12-16 22:13:07 +0900

    [add] login style

---
+++ src/assets/style/User/UserStyle.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const LeftContainer = styled.div`
+  display: flex;
+  width: 50%;
+  height: 100%;
+  background: #ffddd5;
+`;
+export const IconBox = styled.div`
+  height: 100%;
+  display: flex;
+`;
+export const LeftIcon = styled.img`
+  height: 40%;
+  width: 90%;
+`;
+export const LoginContainer = styled.div`
+  display: flex;
+  width: 50%;
+`;
+
+export const LoginBox = styled.div``;
