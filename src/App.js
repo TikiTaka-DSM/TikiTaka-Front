fc1f4fb66ce4331d87db1bc0b7024f8e1bca80aa
김해린 <rin3583@naver.com>
2021-01-06 08:30:45 +0900

    app.js router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Login from "./components/User/Login";
+import {
+  Route,
+  Switch,
+  BrowserRouter as Router,
+  Redirect,
+} from "react-router-dom";
+import SignUp from "./components/User/SignUp";
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <Router>
+      <Switch></Switch>
+      <Redirect path="*" exact to="/login" />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signUp" component={SignUp} />
+    </Router>
   );
 }
 
