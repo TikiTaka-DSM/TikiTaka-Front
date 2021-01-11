078e5488269057ee096660506348d93e6653f66f
김해린 <rin3583@naver.com>
2021-01-11 22:11:37 +0900

    login toast

---
+++ src/lib/Toast.js
@@ -0,0 +1,21 @@
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const Success = (message) => {
+  toast.success(message, {
+    autoClose: 3000,
+  });
+};
+
+export const Error = (message) => {
+  toast.error(message, {
+    autoClose: 2000,
+    position: "top-right",
+  });
+};
+
+export const warning = (message) => {
+  toast.warning(message, {
+    autoClose: 3000,
+  });
+};
