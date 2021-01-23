60dec1fb1478a5976a5e6c15b46249b55347e6ff
김해린 <rin3583@naver.com>
2021-01-23 18:41:59 +0900

    user api edit

--- src/lib/User.js
+++ src/lib/User.js
@@ -1,4 +1,4 @@
-import { getAPI } from "./API";
+import { getAPI, getAccessToken } from "./API";
 
 export const UserJoin = (id, password, name) => {
   return getAPI.post("/user", { id, password, name });
@@ -6,4 +6,24 @@ export const UserJoin = (id, password, name) => {
 
 export const UserLogin = (id, password) => {
   return getAPI.post("/user/auth", { id, password });
+};
+
+export const UserAdd = (id) => {
+  return getAccessToken.post(`/friend/${id}`);
+};
+
+export const UserBlock = (user_id) => {
+  return getAccessToken.delete(`/friend/block/${user_id}`);
+};
+
+export const UserSearch = (user_id) => {
+  return getAPI.get(`/friend?id=${user_id}`);
+};
+
+export const FriendSearch = (user_name) => {
+  return getAccessToken.get(`/friend?name=${user_name}`);
+};
+
+export const FriendList = () => {
+  return getAccessToken.get("/friends");
 };
