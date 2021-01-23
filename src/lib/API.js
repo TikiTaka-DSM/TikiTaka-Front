e2c8ef64d259993a62cfd19f9729503df8248f46
김해린 <rin3583@naver.com>
2021-01-23 18:41:42 +0900

    api form data add

--- src/lib/API.js
+++ src/lib/API.js
@@ -15,6 +15,14 @@ export const getAccessToken = axios.create({
   },
 });
 
+export const getFormData = axios.create({
+  baseURL: "http://54.180.2.226:5000",
+  headers: {
+    "Content-Type": "form-data",
+    Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+  },
+});
+
 // export const getRefreshToken = axios.create({
 //   baseURL: "http://54.180.2.226:5000",
 //   headers: {
