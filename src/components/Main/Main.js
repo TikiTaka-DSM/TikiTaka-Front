4fe2c63c159e3dd0e2c748d6151a158c9c5338ab
김해린 <rin3583@naver.com>
2021-01-25 23:05:56 +0900

    main component edit

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,13 @@
 import React from "react";
 import * as U from "../../assets/style/User/UserStyle";
-
 import ChatList from "./ChatList";
+import Chat from "./ChatView";
 
 const Main = () => {
   return (
     <U.MainContainer>
       <ChatList />
+      <Chat />
     </U.MainContainer>
   );
 };
