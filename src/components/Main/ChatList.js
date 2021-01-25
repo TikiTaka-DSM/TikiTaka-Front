80b7e9a0478139fce499ee03eec796b617ed8329
김해린 <rin3583@naver.com>
2021-01-25 23:01:25 +0900

    chat list

--- src/components/Main/ChatList.js
+++ src/components/Main/ChatList.js
@@ -1,17 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "../../assets/style/Main/MainStyle";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
 import profile from "../../assets/img/imgTest.jpeg";
 import Search from "./Search";
+import { getMyProfile } from "../../lib/Profile";
 const ChatList = () => {
-  useEffect(() => {}, []);
+  let [profile, setProfile] = useState("");
+  const imgUrl = "https://jobits.s3.ap-northeast-2.amazonaws.com/";
+
+  useEffect(() => {
+    getMyProfile().then(() => {
+      setProfile(res.data.profileData.img);
+    });
+  }, []);
   return (
     <>
       <S.ListContainer>
         <S.TopContainer>
           <S.ProfileBox>
-            <S.Profile src={profile} />
+            <S.Profile src={imgUrl + profile} />
           </S.ProfileBox>
           <S.IconBox>
             <FontAwesomeIcon icon={faCog} size="2x" color="white" />
