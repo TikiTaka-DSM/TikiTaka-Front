9fa93c4c693b4dfb732063fb68e6319de00a390d
김해린 <rin3583@naver.com>
2021-01-26 00:34:05 +0900

    img index

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -1,2 +1,6 @@
 export { default as chat } from "./chat.png";
 export { default as logo } from "./logo.png";
+export { default as search } from "./search.png";
+export { default as chatImg } from "./chatImg.png";
+export { default as chatRecord } from "./chatRecord.png";
+export { default as chatSend } from "./chatSend.png";
