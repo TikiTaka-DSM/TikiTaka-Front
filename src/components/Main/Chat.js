820941e07f9a050b8e29fb31a8460b299b2f29d0
김해린 <rin3583@naver.com>
2021-01-29 02:35:12 +0900

    chat component

---
+++ src/components/Main/Chat.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import io from "socket.io-client";
+import * as S from "../../assets/style/Main/ChatStyle";
+
+const socket = io("http://54.180.2.226:5000");
+
+const Chat = () => {
+  let [chat, setChat] = useState("");
+
+  socket.on("test");
+  return <S.Message></S.Message>;
+};
+
+export default Chat;
