cebd5f9c2e7d219895f3add2ada6db5cf9992c4d
김해린 <rin3583@naver.com>
2021-03-02 23:22:54 +0900

    profile edit component

--- src/components/Profile/ProfileEdit.js
+++ src/components/Profile/ProfileEdit.js
@@ -1,10 +1,12 @@
 import React from "react";
 import * as P from "../../assets/style/Profile/ProfileStyle";
 import * as S from "../../assets/style/Profile/EditStyle";
+import { editProfile } from "../../lib/Profile";
+import { edit, testImg } from "../../assets/img";
 
 const ProfileEdit = () => {
   const ProfileChange = () => {
-    editProfile;
+    editProfile();
   };
   return (
     <P.MainContainer>
@@ -12,7 +14,13 @@ const ProfileEdit = () => {
         <S.ImgForm>
           <S.ImgInput id="input-img" />
         </S.ImgForm>
-        <S.ImgBox></S.ImgBox>
+
+        <S.ProfileChange>
+          <S.ImgBox>
+            <S.ProfileImg src={testImg} />
+            <S.Change src={edit} />
+          </S.ImgBox>
+        </S.ProfileChange>
       </P.ProfileBox>
     </P.MainContainer>
   );
