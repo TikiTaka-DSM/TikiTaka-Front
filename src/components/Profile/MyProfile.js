6a74735b997591dce08a409f909706c18ac6cb31
김해린 <rin3583@naver.com>
2021-03-02 23:22:40 +0900

    profile history

--- src/components/Profile/MyProfile.js
+++ src/components/Profile/MyProfile.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import * as S from "../../assets/style/Profile/ProfileStyle";
 import { getMyProfile } from "../../lib/Profile";
+import { useHistory } from "react-router-dom";
 
 const MyProfile = () => {
   let [data, setData] = useState({
@@ -10,7 +11,7 @@ const MyProfile = () => {
     img: "",
   });
   let [length, setLength] = useState(0);
-
+  const history = useHistory();
   const imgSrc = "https://jobits.s3.ap-northeast-2.amazonaws.com/";
 
   useEffect(() => {
@@ -26,6 +27,10 @@ const MyProfile = () => {
     });
     setLength(data.introduce.length);
   }, []);
+
+  const profileEdit = () => {
+    history.push("/edit");
+  };
   return (
     <S.MainContainer>
       <S.ProfileBox>
@@ -35,7 +40,7 @@ const MyProfile = () => {
         <S.Name>{data.name}</S.Name>
         <S.ID>ID : {data.id}</S.ID>
         <S.Introduce value={data.introduce} length={length + length * 13} />
-        <S.EditBut>프로필 변경</S.EditBut>
+        <S.EditBut onClick={profileEdit}>프로필 변경</S.EditBut>
       </S.ProfileBox>
     </S.MainContainer>
   );
