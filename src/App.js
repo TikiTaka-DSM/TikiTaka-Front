49842adef3f9b2f3e46e076e24ff62832d4260e2
김해린 <rin3583@naver.com>
2021-03-09 23:35:33 +0900

    app.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,27 @@ import {
   Redirect,
 } from "react-router-dom";
 import SignUp from "./components/User/SignUp";
+import MyProfile from "./components/Profile/MyProfile";
+import Profile from "./components/Profile/Profile";
+import Main from "./components/Main/Main";
+import ChatView from "./components/Main/ChatView";
+import ProfileEdit from "./components/Profile/ProfileEdit";
 
 function App() {
   return (
     <Router>
-      <Switch></Switch>
-      <Redirect path="*" exact to="/login" />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/signUp" component={SignUp} />
+      <Switch>
+        <>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signUp" component={SignUp} />
+          <Route path="/myProfile" component={MyProfile} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/main" component={Main} />
+          <Route exact path="/main/chat" component={ChatView} />
+          <Route exact path="/edit" component={ProfileEdit} />
+          {/* <Redirect path="*" exact to="/login" /> */}
+        </>
+      </Switch>
     </Router>
   );
 }
