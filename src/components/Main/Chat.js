50aba32705ee71ddc0a3ef111a8b90cdaf375037
김해린 <rin3583@naver.com>
2021-03-09 23:35:20 +0900

    chat message style

--- src/components/Main/Chat.js
+++ src/components/Main/Chat.js
@@ -1,14 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import * as S from "../../assets/style/Main/ChatStyle";
 
-const socket = io("http://54.180.2.226:3003");
+// const socket = io("http://54.180.2.226:3003");
 
 const Chat = () => {
-  let [chat, setChat] = useState("");
+  let [chat, setChat] = useState(
+    "안녕하세요. 대덕소프트웨어 마이스터 고등학교 소프트웨어 개발과 3학년 1반 5번 김해린입니다."
+  );
+  let [length, setLength] = useState({
+    width: 0,
+    height: 0,
+  });
 
-  socket.on("test");
-  return <S.Message></S.Message>;
+  useEffect(() => {
+    setLength({
+      width: chat.length,
+      height: chat.length,
+    });
+  }, []);
+  return (
+    <S.MessageLayOut>
+      <S.MyMessageBox width={length.width} height={length.height}>
+        <S.Message> {chat}</S.Message>
+      </S.MyMessageBox>
+      <S.MessageBox width={length.width} height={length.height}>
+        <S.Message>{chat}</S.Message>
+      </S.MessageBox>
+    </S.MessageLayOut>
+  );
 };
 
 export default Chat;
